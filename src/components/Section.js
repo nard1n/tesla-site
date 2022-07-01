6b4792aa7a3fec146e10dc27261af802ee08a28c
Nardin Lachowski <nardin@live.com>
2022-06-30 21:44:48 -0400

    added media to accomodate flex direction on smaller screens

--- src/components/Section.js
+++ src/components/Section.js
@@ -49,6 +49,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
     display: flex;
     margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `
 
 const LeftButton = styled.div`
@@ -68,12 +71,14 @@ const LeftButton = styled.div`
 `
 
 const RightButton = styled(LeftButton)`
-
+    background: white;
+    opacity: 0.65;
+    color: black;
 `
 
 const DownArrow = styled.img`
-    margin-top: 20px;
     height: 40px;
+    overflow-x: hidden;
     animation: animateDown infinite 1.5s;
 `
 
