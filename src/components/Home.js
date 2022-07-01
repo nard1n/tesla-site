13e4b5e7b5e5b4b07fefd56f426a156efd002264
Nardin Lachowski <nardin@live.com>
2022-07-01 19:14:18 -0400

    added the different sections with the appropriate props to be passed

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,57 @@ import React from 'react';
 import styled from "styled-components";
 import Section from './Section';
 
-function Home() {
+function Home({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
   return (
     <Container>
-        <Section />
-        <Section />
-        <Section />
-        <Section />
+        <Section 
+            title="Model 3"
+            description="Order Online for Touchless Delivery"
+            backgroundImg="model-3.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"
+        />
+            <Section 
+            title="Model Y"
+            description="Order Online for Touchless Delivery"
+            backgroundImg="model-y.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"
+        />
+        <Section 
+            title="Model S" 
+            description="Order Online for Touchless Delivery"
+            backgroundImg="model-s.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"
+        />
+        <Section 
+            title="Model X"
+            description="Order Online for Touchless Delivery"
+            backgroundImg="model-x.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"
+        />
+         <Section 
+            title="Solar Panels"
+            description="Lowest Cost Panels in America"
+            backgroundImg="solar-panel.jpg"
+            leftBtnText="Order Now"
+            rightBtnText="Learn More"
+        />
+        <Section 
+            title="Solar Roof"
+            description="Reduce Clean Energy From Your Roof"
+            backgroundImg="solar-roof.jpg"
+            leftBtnText="Order Now"
+            rightBtnText="Learn More"
+        />
+        <Section 
+            title="Accessories"
+            description=""
+            backgroundImg="accessories.jpg"
+            leftBtnText="Shop Now"
+        />
     </Container>
 
   )
